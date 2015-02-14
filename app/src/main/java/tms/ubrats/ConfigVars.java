5c2eac66bb742c6c868895f22a130732b14b0ee0
Thomas Binu <thomas.binu@gistec.com>
2015-02-14 23:53:21 +0400

    Initial commit

---
+++ app/src/main/java/tms/ubrats/ConfigVars.java
@@ -0,0 +1,7 @@
+package tms.ubrats;
+
+/**
+ * Created by tmsbn on 2/14/15.
+ */
+public class ConfigVars {
+}
---
+++ app/src/main/java/tms/ubrats/Networking.java
@@ -0,0 +1,44 @@
+package tms.ubrats;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.android.AndroidLog;
+import retrofit.converter.GsonConverter;
+import retrofit.http.GET;
+
+public class Networking {
+
+    private static final String URL = "http://temp/restpoint";
+    private static RestInterface sRestService;
+
+    public static RestInterface getRestClient() {
+        if (sRestService == null) {
+
+            String DATE_FORMAT = "dd/MM/yyyy";
+            Gson gson = new GsonBuilder()
+                    .setDateFormat(DATE_FORMAT)
+                    .create();
+
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint(URL)
+                    .setConverter(new GsonConverter(gson))
+                    .setLogLevel(RestAdapter.LogLevel.FULL)
+                    .setLog(new AndroidLog("retrofit logs"))
+                    .build();
+            sRestService = restAdapter.create(RestInterface.class);
+        }
+        return sRestService;
+
+    }
+
+    public interface RestInterface {
+        @GET("/config.json")
+        void getConfig(Callback<ConfigVars> callback);
+
+
+    }
+}
